7e02ca8cd31ffc5c0ee28609d9e93a322d79f277
Sunny <sunnysagar620@gmail.com>
2025-05-05 17:29:29 +0530

    commit updated ui and code

--- app/src/main/java/com/example/mangafaceapp/presentation/ui/MangaItem.kt
+++ app/src/main/java/com/example/mangafaceapp/presentation/ui/MangaItem.kt
@@ -8,11 +8,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -23,15 +29,35 @@ import com.example.mangafaceapp.data.model.Manga
 @Composable
 fun MangaItem(manga: Manga, onClick: () -> Unit) {
 
-        Column(modifier = Modifier
+    var imageLoadFailed by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
             .clickable { onClick() }
-            .padding(4.dp)) {
+            .padding(4.dp)
+    ) {
+        if (imageLoadFailed) {
+            manga.title?.let {
+                Text(
+                    text = it,
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier
+                        .size(150.dp)
+                        .wrapContentSize(Alignment.Center)
+                )
+            }
+        } else {
             AsyncImage(
-                model = manga.thumb?: "https://via.placeholder.com/150",
+                model = manga.thumb ?: "https://via.placeholder.com/150",
                 contentDescription = manga.title,
-                modifier = Modifier.size(150.dp)
+                modifier = Modifier.size(150.dp),
+                onError = {
+                    imageLoadFailed = true
+                }
             )
         }
+    }
+
 
 }
